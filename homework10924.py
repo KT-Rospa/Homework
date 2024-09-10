76618ef9c13d2d9c4254410627118c812dd5ebdb
KT-Rospa <ctw20.kacper.trela@corketb.ie>
2024-09-10 21:36:04 +0100

    Add files via upload

---
+++ homework10924.py
@@ -0,0 +1,51 @@
+# Author: Kacper Trela
+# Date: Sep 2024
+
+# Task 1 - pg18
+
+age = 16
+print("You're", age, "What a great age!")
+
+#  Task 2
+
+num1 = 7.4
+num2 = 3.2
+
+print(num1)
+print(num2)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+
+
+# Task 3
+
+x = 1.6
+y = 2.2
+
+average = x+y/2
+print("The average is", average)
+
+divide = x/y
+print(divide)
+
+power = x**y
+print(power)
+
+# Task 4
+
+fahrenheit = float(input("Enter Fahrenheit value: " ))
+celcius = round((5/9) * (fahrenheit - 32))
+print(fahrenheit, "degrees F =", celcius, "degrees C")
+
+#  Task 5
+
+# I am 7082mi from Singapore
+
+mile = float(input("How many miles are you from Singapore?: "))
+kilo = round(mile * 1.60935)
+print("You are", kilo, "kilometers from Singpore")
+
+print("That'll cost you, only €",kilo * 900)
+             
