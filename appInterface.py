2632b474b1afb1b3150c0694a4e6c6f3df146d86
KT-Rospa <ctw20.kacper.trela@corketb.ie>
2024-09-15 21:21:08 +0100

    Add files via upload

---
+++ appInterface.py
@@ -0,0 +1,29 @@
+#Author: Kacper Trela
+#Date: Sept 2024
+#App interface homework
+
+
+print("\t _______________________________")
+print("\t|Alarm:\t\t\t\t|")
+print("\t|\t\t\t\t|")
+print("\t| 06:30 ❌ - Sound: Opening 🔽\t|")
+print("\t| 06:50 ✅ - Sound: Custom 🔽\t|")
+print("\t| 07:00 ✅ - Sound: Custom 🔽\t|")
+print("\t| 07:30 ❌ - Sound: Apex 🔽\t|")
+print("\t| 10:00 ❌ - Sound: Apex 🔽\t|")
+print("\t| 11:00 ❌ - Sound: Radar 🔽\t|")
+print("\t| 15:00 ❌ - Sound: Apex 🔼\t|")
+print("\t|_______________________________|") 
+print("\t| 📅 Schedule alarm\t     + \t|")
+print("\t| 🎵 Sound - Apex\t     +\t|")
+print("\t| 📳 Vibrate\t\t    ✅\t|")
+print("\t| 🗑 Delete\t\t\t|")
+print("\t|_______________________________|")
+print("\t|\t\t\t\t|")
+print("\t| 17.00 ❌ - Sound: Apex 🔼\t|")
+print("\t| 17.15 ❌ - Sound: Apex 🔼\t|")
+print("\t| 22.30 ❌ - Sound: Radar 🔼\t|")
+print("\t|_______________________________|")
+print("\t|\t\t\t\t|")
+print("\t|\t\t➕\t\t|")
+print("\t|_______________________________|")
