a6619c0df16d49a233bb250b18e66ae0507e90ea
KT-Rospa <ctw20.kacper.trela@corketb.ie>
2024-09-28 21:37:01 +0100

    Add files via upload

---
+++ HWpg42.py
@@ -0,0 +1,33 @@
+#Author: Kacper Trela
+#Date: Sept 2024
+#HW: Task 1-2 pg 42
+
+#Task 1:
+# num1 = int(input("Pick a number: "))
+# num2 = int(input("Pick a number: "))
+# num3 = int(input("Pick a number: "))
+# num4 = int(input("Pick a number: "))
+# num5 = int(input("Pick a number: "))
+# 
+# numList = [num1, num2, num3, num4, num5]
+# 
+# print(numList)
+# 
+# numb1 =+ num1 + 1
+# numb2 =+ num2 + 1
+# numb3 =+ num3 + 1
+# numb4 =+ num4 + 1
+# numb5 =+ num5 + 1
+# 
+# numbList = [numb1, numb2, numb3, numb4, numb5]
+# print(numbList)
+
+#Task 2:
+
+hours = [12, 7, 9, 9, 6, 8, 2]
+print("Stephen drank", sum(hours)*0.5, "liters of milk during that 7-day period.")
+
+print("Stephen would have to pay", round(sum(hours)*0.5*1.35,2), "euro to his dad at the end of the week.")
+
+
+
